d83f4bd19b08401759af9d11e93a5d70d1cbfe72
wingsoflight <wingsoflight2003@gmail.com>
2017-10-11 17:50:15 +0600

    truth mask setting

--- main.py
+++ main.py
@@ -1,28 +1,39 @@
 import cv2
 import numpy as np
 
+def F_l(_lt_f, X):
+    return _lt_f == X
+
+def F_s(_st_f, X):
+    return _st_f == X
+
+def calcE(_E, _lt_f, _st_f, X):
+    fl_tm = F_l(_lt_f, X)
+    fs_tm = F_s(_st_f, X)
+    fl_fm = np.invert(fl_tm)
+    fs_fm = np.invert(fs_tm)
+    _E[fl_tm & fs_fm] += 1
+    _E[fl_fm | fs_tm] -= k
+    _E[_E > max_e] = max_e
+    _E[_E < 0] = 0
+    return _E
 
 if __name__ == '__main__':
-    camera = cv2.VideoCapture(0)
-    alpha_short_term = 1
-    alpha_long_term = .01
+    camera = cv2.VideoCapture('video1.avi')
     background = camera.read()[1]
-    lt_img = background.copy()
-    st_img = background.copy()
-    n = 0
+    lt_f = background
+    st_f = background
+    E = np.zeros((background.shape))
+    k = 300
+    max_e = 300
     while 1:
         ret, frame = camera.read()
         if not ret:
             raise Exception('No signal')
-        lt_img = lt_img.astype(np.float64)
-        st_img = st_img.astype(np.float64)
-        frame = frame.astype(np.float64)
-        lt_img += (lt_img - frame) * alpha_long_term
-        frame = frame.astype(np.uint8)
-        lt_img = lt_img.astype(np.uint8)
-        cv2.imshow('Short term foreground', st_img)
-        cv2.imshow('Long term foreground', lt_img)
-        key = cv2.waitKeyEx(1)
+        E = calcE(E, lt_f, st_f, frame)
+        #print(E.sum())
+        cv2.imshow('Frame', frame)
+        key = cv2.waitKeyEx(int(1000 / camera.get(cv2.CAP_PROP_FPS)))
         if key == ord('q'):
             break
     camera.release()
