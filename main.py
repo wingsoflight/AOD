cdb567ac6d339e86797401eaf42e2871fa42fa0c
wingsoflight <wingsoflight2003@gmail.com>
2017-10-19 16:00:41 +0600

    Detecting abandoned object using dual foregrounds

--- main.py
+++ main.py
@@ -1,47 +1,46 @@
 import cv2
 import numpy as np
 
-def cmp_foreground(foreground, X):
-    return np.take(foreground, -1, 2) != np.take(X, -1, 2)
-
-def calcE(frame):
-    mask = cmp_foreground(np.zeros((frame.shape)), frame)
-    nmask = np.invert(mask)
-    E[mask] += 1
-    E[nmask] -= k
-    E[E > max_e] = max_e
-    E[E < 0] = 0
+def calcE():
+    ltf_tm = lt_f.astype(np.bool)
+    stf_tm = st_f.astype(np.bool)
+    ltf_fm = np.invert(ltf_tm)
+    stf_fm = np.invert(stf_tm)
+    E[ltf_tm & stf_fm] += 1;
+    E[ltf_fm | stf_tm] -= k;
+    E[E < 0] = 0;
+    E[E > max_e] = max_e;
 
 if __name__ == '__main__':
     camera = cv2.VideoCapture('video1.avi')
-    camera.set(cv2.CAP_PROP_FPS, 25.0)
-    background = camera.read()[1]
-    background = cv2.GaussianBlur(background, (5, 5), 0)
-    lt_f = background
-    st_f = background
-    E = np.zeros((background.shape[0], background.shape[1]))
-    k = 255
-    max_e = 255
-    cnt = 0
+    ret, sample = camera.read();
+    if not ret:
+        raise Exception('No signal')
+    E = np.zeros((sample.shape[0], sample.shape[1]))
+    bg = np.zeros(shape=sample.shape, dtype=np.uint8)
+    k = 300
+    max_e = 300
+    lt_bs = cv2.createBackgroundSubtractorMOG2(1, 900, False)
+    st_bs = cv2.createBackgroundSubtractorMOG2(1, 200, False)
     while 1:
         ret, frame = camera.read()
         if not ret:
             raise Exception('No signal')
-        _frame = cv2.GaussianBlur(frame, (5, 5), 0)
-        _frame = cv2.absdiff(_frame, background)
-        _, _frame = cv2.threshold(_frame, 25, 255, cv2.THRESH_BINARY)
-        calcE(_frame)
-        _, _E = cv2.threshold(E, 250, 255, cv2.THRESH_BINARY)
+        lt_f = lt_bs.apply(frame, bg, learningRate=0.0001)
+        st_f = st_bs.apply(frame, bg, learningRate=0.01)
+        calcE();
+        _, _E = cv2.threshold(E, 299, 255, 0)
         _E = _E.astype(np.uint8)
-        (_, cnts, _) = cv2.findContours(_E.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        _, cnts, _ = cv2.findContours(_E, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
         for c in cnts:
-            if cv2.contourArea(c) < 900:
+            if cv2.contourArea(c) < 256:
                 continue
             (x, y, w, h) = cv2.boundingRect(c)
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-        cv2.imshow('E', _E)
-        cv2.imshow('Frame', frame)
-        key = cv2.waitKeyEx(18)
+        cv2.imshow('LongTerm', lt_f)
+        cv2.imshow('ShortTerm', st_f)
+        cv2.imshow('Original', frame)
+        key = cv2.waitKeyEx(15)
         if key == ord('q'):
             break
     camera.release()
